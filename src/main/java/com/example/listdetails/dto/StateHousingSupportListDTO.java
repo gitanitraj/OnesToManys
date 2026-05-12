8789ca9f8a0f11f9882db1a4a374b2ea82efa0f3
gitanitraj <anitra.com@gmail.com>
2026-05-12 08:50:06 -0400

    Added DTO folder and code; Added service folder

---
+++ src/main/java/com/example/listdetails/dto/StateHousingSupportListDTO.java
@@ -0,0 +1,18 @@
+package com.example.listdetails.dto;
+
+public class StateHousingSupportListDTO {
+
+    private Long id;
+    private String stateName;
+    private String agencyName;
+
+    public StateHousingSupportListDTO(Long id, String stateName, String agencyName) {
+        this.id = id;
+        this.stateName = stateName;
+        this.agencyName = agencyName;
+    }
+
+    public Long getId() { return id; }
+    public String getStateName() { return stateName; }
+    public String getAgencyName() { return agencyName; }
+}
