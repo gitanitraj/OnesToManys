cb9dc4f77994389e1878253f15821c80c5d691f8
gitanitraj <anitra.com@gmail.com>
2026-05-12 09:27:20 -0400

    Added MortgageProgram DTO code

---
+++ src/main/java/com/example/listdetails/dto/MortgageProgramListDTO.java
@@ -0,0 +1,18 @@
+package com.example.listdetails.dto;
+
+public class MortgageProgramListDTO {
+    private Long id;
+    private String programName;
+    private Double interestRate;
+
+    public MortgageProgramListDTO(Long id, String programName, Double interestRate) {
+        this.id = id;
+        this.programName = programName;
+        this.interestRate = interestRate;
+    }
+
+    public Long getId() { return id; }
+    public String getProgramName() { return programName; }
+    public Double getInterestRate() { return interestRate; }
+    
+}
