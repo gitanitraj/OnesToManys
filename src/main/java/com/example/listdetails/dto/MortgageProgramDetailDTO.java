5623d91468c59effb53ed8d5215f3b97259615dc
gitanitraj <anitra.com@gmail.com>
2026-05-12 11:22:47 -0400

    Added MortgageProgramDetailDTO

---
+++ src/main/java/com/example/listdetails/dto/MortgageProgramDetailDTO.java
@@ -0,0 +1,14 @@
+package com.example.listdetails.dto;
+
+public class MortgageProgramDetailDTO {
+
+    private Long id;
+    private Long stateHousingSupportId;
+    private String programName;
+    private Double interestRate;
+    private Double maxLoanAmount;
+    private Integer termYears;
+    private Boolean active;
+
+    // getters and setters
+}
