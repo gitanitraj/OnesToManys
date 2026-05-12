cb0cf652e68e625fef07de6dc879063d326e7043
gitanitraj <anitra.com@gmail.com>
2026-05-12 11:36:47 -0400

    MortgageProgramDetailDTO added

--- src/main/java/com/example/listdetails/dto/MortgageProgramDetailDTO.java
+++ src/main/java/com/example/listdetails/dto/MortgageProgramDetailDTO.java
@@ -11,4 +11,26 @@ public class MortgageProgramDetailDTO {
     private Boolean active;
 
     // getters and setters
+    public MortgageProgramDetailDTO() {}
+
+    public Long getID() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getStateHousingSupportId() { return stateHousingSupportId; }
+    public void setStateHousingSupportId(Long stateHousingSupportId) { this.stateHousingSupportId = stateHousingSupportId; }
+
+    public String getProgramName() { return programName; }
+    public void setProgramName(String programName) { this.programName = programName; }
+
+    public Double getInterestRate() { return interestRate; }
+    public void setInterestRate(Double interestRate) { this.interestRate = interestRate; }
+
+    public Double getMaxLoanAmount() { return maxLoanAmount; }
+    public void setMaxLoanAmount(Double maxLoanAmount) { this.maxLoanAmount = maxLoanAmount; }
+
+    public Integer getTermYears() { return termYears; }
+    public void setTermYears(Integer termYears) { this.termYears = termYears; }
+
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
 }
