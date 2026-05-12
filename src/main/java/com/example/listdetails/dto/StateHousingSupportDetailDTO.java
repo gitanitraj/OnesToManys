d904ec244d145f38c633164c22d9654745b7776c
gitanitraj <anitra.com@gmail.com>
2026-05-12 09:20:31 -0400

    Added DTO code

---
+++ src/main/java/com/example/listdetails/dto/StateHousingSupportDetailDTO.java
@@ -0,0 +1,25 @@
+package com.example.listdetails.dto;
+
+public class StateHousingSupportDetailDTO {
+
+        private Long id;
+        private String stateName;
+        private String agencyName;
+        private String website;
+        private String phone;
+
+        public StateHousingSupportDetailDTO(Long id, String stateName, String agencyName, String website, String phone) {
+            this.id = id;
+            this.stateName = stateName;
+            this.agencyName = agencyName;
+            this.website = website;
+            this.phone = phone;
+        }
+
+        public Long getId() { return id; }
+        public String getStateName() { return stateName; }
+        public String getAgencyName() { return agencyName; }
+        public String getWebsite() { return website; }
+        public String getPhone() { return phone; }
+}
+
