89f250ab1cf978455dfab8da109c023e26a8b7c2
gitanitraj <anitra.com@gmail.com>
2026-05-13 08:24:48 -0400

    Updated StateHousingSupportListDTO.java to match service/controller design

--- src/main/java/com/example/listdetails/dto/StateHousingSupportListDTO.java
+++ src/main/java/com/example/listdetails/dto/StateHousingSupportListDTO.java
@@ -6,13 +6,23 @@ public class StateHousingSupportListDTO {
     private String stateName;
     private String agencyName;
 
+    public StateHousingSupportListDTO() {}
+
     public StateHousingSupportListDTO(Long id, String stateName, String agencyName) {
         this.id = id;
         this.stateName = stateName;
         this.agencyName = agencyName;
     }
 
-    public Long getId() { return id; }
-    public String getStateName() { return stateName; }
-    public String getAgencyName() { return agencyName; }
+    public Long getId() {
+        return id;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
 }
