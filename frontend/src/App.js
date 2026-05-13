49da8ae3e9ac5c56cfb875dee59f594c67930b5f
gitanitraj <anitra.com@gmail.com>
2026-05-13 10:54:41 -0400

    Added imports to JS app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ProgramList from "./components/ProgramList";
+import ProgramDetail from "./components/ProgramDetail";
+import ProgramForm from "./components/ProgramForm";
+
+
 function App() {
   return (
     <div className="App">
